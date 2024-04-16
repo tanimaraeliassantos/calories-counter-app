76859db38170aa23276de86e1ae5e4b6c4df81a5
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-16 21:59:59 +0200

    Update script.js
    Tested out function by printing output on console

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
+	console.log('original string: ', str);
 	const regex = str.replace(/[+\s]/g, '');
 	return str.replace(regex, '');
 }
