98fc9ecaaca23faef7ccdedb7a0f3016bf48386b
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-23 14:30:06 +0200

    Update script.js
    Removed declaration of variable and susbstitute it within template literal

--- script.js
+++ script.js
@@ -18,8 +18,7 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-	const targetId = '#' + entryDropdown.value;
 	const targetInputContainer = document.querySelector(
-		`${targetId} .input-container`
+		`#${entryDropdown.value} .input-container`
 	);
 }
