b4d58f9785d17e5adf02216cac44113006169715
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-24 21:55:36 +0200

    Update script.js
    Added input element to HTML String with type number to receive Calories number

--- script.js
+++ script.js
@@ -27,5 +27,6 @@ function addEntry() {
 			Entry ${entryNumber} Name
 		</label> 
 		<input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
-		<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber}</label>`;
+		<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber}</label>
+		<input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>`;
 }
