8d8e613b1b93309cfaf33ec01e27dec99b035dfc
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-24 22:10:57 +0200

    Update script.js
    Appended HTMLString to targetInputContainer as inner HTML

--- script.js
+++ script.js
@@ -29,4 +29,5 @@ function addEntry() {
 		<input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
 		<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber}</label>
 		<input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>`;
+		targetInputContainer.innerHTML += HTMLString;
 }
