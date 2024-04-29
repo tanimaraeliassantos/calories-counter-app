d68bedfb7d884c534db60bea73a7ff7f9421124e
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-04-29 15:33:37 +0200

    Update script.js
    Checked to see if invalidInputMatch is truthy

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function getCaloriesFromInputs(list) {
 	for (const item of list) {
 		const currVal = cleanInputString(item.value);
 		const invalidInputMatch = isInvalidInput(currVal);
+		if (invalidInputMatch) {
+		}
 	}
 }
 
