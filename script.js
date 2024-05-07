bf606454be479c0ec99d0c88b3eede9ca1d0fd68
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-05-07 12:30:37 +0200

    Update script.js
    Created function calculateCalories

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ function addEntry() {
 	targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+
+}
+
 function getCaloriesFromInputs(list) {
 	let calories = 0;
 	for (const item of list) {
