af520b5b04c2ec7f08543e21451a0d71394dba45
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-05-14 14:39:41 +0200

    Update script.js
    Added a new line to span created on output element

--- script.js
+++ script.js
@@ -69,7 +69,10 @@ function calculateCalories(e) {
 		budgetCalories - consumedCalories + exerciseCalories;
 	const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : "Deficit'";
 
-	output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>`;
+	output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+		remainingCalories
+	)} Calorie ${surplusOrDeficit}</span>
+	<hr>`;
 }
 
 function getCaloriesFromInputs(list) {
