1f1f869f1da4a9c0cfe7418b6ae478c62c8af284
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-05-14 12:43:55 +0200

    Update script.js
    Added span with class defined by variable surplusOrDeficit

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function calculateCalories(e) {
 		budgetCalories - consumedCalories + exerciseCalories;
 	const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : "Deficit'";
 
-	output.innerHTML = ``;
+	output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}"></span>`;
 }
 
 function getCaloriesFromInputs(list) {
