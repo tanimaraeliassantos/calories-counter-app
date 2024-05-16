f97a02df1a348b2bd5cfabfdccd7809502bc256f
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-05-16 12:53:52 +0200

    Update script.js
    Set budgetNumberInput to an empty string after the for loop completes

--- script.js
+++ script.js
@@ -100,6 +100,7 @@ function clearForm() {
 	for (const container of inputContainers) {
 		container.innerHTML = '';
 	}
+	budgetNumberInput.value = '';
 }
 
 addEntryButton.addEventListener('click', addEntry);
